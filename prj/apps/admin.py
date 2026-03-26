0828adf6fbe63f2e2e5adf931b42fda862d47296
Filip Kadlec <filip.kadlec.s@gyarab.cz>
2026-03-26 21:39:09 +0100

    Clean up admin.py by removing comments
    Removed comments from UzivatelAdmin and RezervaceAdmin classes.

--- prj/apps/admin.py
+++ prj/apps/admin.py
@@ -3,11 +3,8 @@ from .models import Uzivatel, Auto, Rezervace
 
 @admin.register(Uzivatel)
 class UzivatelAdmin(admin.ModelAdmin):
-    # Sloupečky, které se ukážou v seznamu
     list_display = ('id', 'jmeno', 'email', 'role')
-    # Podle čeho půjde filtrovat v pravém panelu
     list_filter = ('role',)
-    # V jakých polích bude vyhledávat horní políčko
     search_fields = ('jmeno', 'email')
 
 @admin.register(Auto)
@@ -20,5 +17,4 @@ class AutoAdmin(admin.ModelAdmin):
 class RezervaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'uzivatel', 'auto', 'datum_vyzvednuti', 'misto_vraceni', 'typ_pojisteni', 'stav')
     list_filter = ('stav', 'typ_pojisteni', 'datum_vyzvednuti')
-    # Můžeme vyhledávat i pomocí vztahů (např. jméno uživatele nebo SPZ auta)
     search_fields = ('uzivatel__jmeno', 'auto__spz', 'misto_vraceni')
